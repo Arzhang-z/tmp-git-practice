8de5cb4e57a24dd70a3cba9ca3997354cc793fb4
M.M.ArzhangZade <m.m.arjang.z@gmail.com>
2025-12-23 22:41:12 +0330

    Branch check

--- src/main.py
+++ src/main.py
@@ -4,3 +4,13 @@ import numpy as np
 
 print("Hello world")
 print("1,23,,4,")
+
+def check_main():
+	if __name__ == ' __main__':
+		return  True
+	else:
+		return  False
+a =  check_main()
+assert a != True
+
+#assert a == True
